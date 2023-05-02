679f69b42b41778bb7feaba68b3aa1bc75bda6b0
Aditya Agarwal <aditya.agarwal1311@gmail.com>
2023-05-02 18:01:52 +0530

    Database Online and Environment variables

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const https = require("https")
 const port = process.env.PORT || 3000
 const mongoose = require("mongoose")
+const dotenv = require('dotenv')
+
+dotenv.config()
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -10,7 +15,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB")
+mongoose.connect("mongodb+srv://aditya:201B015@daily-journal.ar5mtft.mongodb.net/?retryWrites=true&w=majority")
 
 // let posts = [];
 
@@ -42,9 +47,6 @@ app.get("/", (req, res) => {
 
     })
   })
-
-    
-
 })
 
 app.get("/posts/:postId", (req, res) => {
